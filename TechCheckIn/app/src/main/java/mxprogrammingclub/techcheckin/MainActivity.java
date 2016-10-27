36adfcf2a25f01a52f3cb1215fc860bb0449e62f
juliamcclellan <jmcclellan@mxschool.edu>
2016-10-27 13:54:29 -0400

    Setting up estimot

--- TechCheckIn/app/src/main/java/mxprogrammingclub/techcheckin/MainActivity.java
+++ TechCheckIn/app/src/main/java/mxprogrammingclub/techcheckin/MainActivity.java
@@ -3,11 +3,21 @@ package mxprogrammingclub.techcheckin;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.estimote.sdk.SystemRequirementsChecker;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //from http://developer.estimote.com/android/tutorial/part-1-setting-up/
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
     }
 }
