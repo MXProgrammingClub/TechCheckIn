c41405cd2c1bb32454c2b91497a857e45ad42497
juliamcclellan <jmcclellan@mxschool.edu>
2016-11-08 18:31:53 -0500

    UI changes

--- TechCheckIn/app/src/main/java/mxprogrammingclub/techcheckin/MainActivity.java
+++ TechCheckIn/app/src/main/java/mxprogrammingclub/techcheckin/MainActivity.java
@@ -2,10 +2,15 @@ package mxprogrammingclub.techcheckin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.estimote.sdk.SystemRequirementsChecker;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity
+{
+    private Button button = (Button)findViewById(R.id.button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,10 @@ public class MainActivity extends AppCompatActivity {
     {
         super.onResume();
         SystemRequirementsChecker.checkWithDefaultDialogs(this);
+    }
+
+    public void toggleEnter(boolean entered)
+    {
+        button.setEnabled(entered);
     }
 }
